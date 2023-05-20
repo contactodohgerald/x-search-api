21cc2c34f56b24b61e91d28633a46897853080dc
Gerald Odoh <contactodohgerald@gmail>
2023-05-20 15:52:27 +0100

    changed auth middleware to return readable codes

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -5,17 +5,17 @@ const authenticateToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1]
   
     if (token == null)
-        return res.status(401).send({message: 'Not Authorized' });
+        return res.status(401).json({message: 'Not Authorized' });
   
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err instanceof jwt.TokenExpiredError)
-            return res.status(401).send({message: 'Unauthorized! Access Token was expired!' });
+            return res.status(401).json({message: 'Unauthorized! Access Token was expired!' });
         
         if (err instanceof jwt.JsonWebTokenError)
-            return res.status(401).send({message: 'Unauthorized! Invalid Token!' });
+            return res.status(401).json({message: 'Unauthorized! Invalid Token!' });
         
         if (err)
-            return res.status(401).send({message: 'Not Authenticated'})
+            return res.status(401).json({message: 'Not Authenticated'})
         
         req.userID = user.uuid;
         req.email = user.email;
